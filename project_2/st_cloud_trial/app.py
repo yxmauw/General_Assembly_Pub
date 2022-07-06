014ff883758ce06a46143cfbf1ae84614254d32c
yxmauw <103557809+yxmauw@users.noreply.github.com>
2022-07-06 12:45:26 +0800

    Update app.py

--- project_2/st_cloud_trial/app.py
+++ project_2/st_cloud_trial/app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from model_methods import predict
+from modules.model_methods import predict
 
 # configuration of the page
 st.set_page_config(
