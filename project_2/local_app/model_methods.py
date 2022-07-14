7ab588fa55984433e81e1e91b30a34bcee5f7969
yxmauw <103557809+yxmauw@users.noreply.github.com>
2022-07-14 15:10:22 +0800

    Update model_methods.py

--- project_2/local_app/model_methods.py
+++ project_2/local_app/model_methods.py
@@ -11,8 +11,8 @@ def predict(new_data):
     imp_data = pd.read_csv('./streamlit_imp_data.csv')
     imp = KNNImputer()
     imp.fit(imp_data)
-    shaped_data = np.reshape(new_data, (1, -1))
-    input_data = imp.transform(shaped_data)
+    shaped_data = np.reshape(new_data, (1, -1)) # to configure data format as if it is a new row to existing data
+    input_data = imp.transform(shaped_data) # impute missing value
     # load model
     with open('final_model.sav','rb') as f:
         model = pickle.load(f)
